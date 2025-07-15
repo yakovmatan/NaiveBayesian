d5b0bfe2b661eb5f950f6926e6d85703ca493060
yakov matan <yakovmatan@gmail.com>
2025-07-15 15:22:08 +0300

    fix

--- prediction/evalution.py
+++ prediction/evalution.py
@@ -30,22 +30,17 @@ class ModelTester:
         X = df.drop(columns=[self.model.classified])
         y = df[self.model.classified]
 
-        # הפיכת כל הערכים למחרוזות כדי לא להתנגש עם Naive Bayes שלך
         X = X.astype(str)
 
-        # חלוקה
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
 
-        # בניית df חדש רק מהחלק של האימון
         train_df = X_train.copy()
         train_df[self.model.classified] = y_train
 
-        # בנה מודל חדש על דאטה מאומן בלבד
         new_model = type(self.model)(train_df, self.model.classified)
         new_model.fit()
         predictor = Prediction(new_model)
 
-        # חיזוי על כל ה־X_test
         correct = 0
         total = len(X_test)
 
